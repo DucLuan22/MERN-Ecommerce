02196aa89506a056c534f7703818edd7fbcfb84c
Dang Duc Luan <ducluandang22@gmail.com>
2022-07-24 20:02:32 +0700

    First commit

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import ProductPage from "./pages/ProductPage";
+import Cart from "./pages/ProductPage";
+function App() {
+  const location = useLocation();
+  const noNav = ["/cart"];
+  return (
+    <>
+      {!noNav.includes(location.pathname) && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/product/:product_id" element={<ProductPage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
