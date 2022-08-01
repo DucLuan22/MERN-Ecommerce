13f98d321f2d971d5e4ba747ada924a13a1c2747
Dang Duc Luan <ducluandang22@gmail.com>
2022-08-01 15:49:56 +0700

    Add routes and models for backend

---
+++ backend/models/Product.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+const productSchema = mongoose.Schema({
+  productName: { type: String, required: true },
+  price: { type: Number, required: true },
+  stock: { type: Number, required: true },
+});
+module.exports = mongoose.model("product", productSchema);
