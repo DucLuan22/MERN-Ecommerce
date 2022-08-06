dd13b8628d24d9eaab7bbe6fe34fac0c448be878
Dang Duc Luan <ducluandang22@gmail.com>
2022-08-06 22:40:58 +0700

    Add RESTAPI for product, category, brand. Add redux states for sidebar.

--- client/src/pages/admin/Dashboard.js
+++ client/src/pages/admin/Dashboard.js
@@ -2,8 +2,7 @@ import React from "react";
 import Sidebar from "../../components/Sidebar";
 export const Dashboard = () => {
   return (
-    <div className="flex">
-      <Sidebar />
+    <div className="flex ml-24 ">
       <div>Home page</div>
     </div>
   );
