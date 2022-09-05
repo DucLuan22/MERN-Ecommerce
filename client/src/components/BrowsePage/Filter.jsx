39b7d3d4da13b2367a462988b4b8cee1a78781e7
Dang Duc Luan <ducluandang22@gmail.com>
2022-09-05 22:56:36 +0700

    Added browse page

---
+++ client/src/components/BrowsePage/Filter.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Filter = () => {
+  return (
+    <section className="sm:col-span-3 lg:col-span-2 bg-[#F9F9F9] border-r-[2px] border-">
+      <div className="flex p-2 border-b-[1px] border-[#c4c3c3]">
+        <h1 className=" text-3xl font-bold text-[#252121] tracking-widest">
+          Filter
+        </h1>
+      </div>
+    </section>
+  );
+};
