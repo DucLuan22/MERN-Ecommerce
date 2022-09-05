789648d0be46ee0695e1cd86dfa3a1c4d88e0b45
Dang Duc Luan <ducluandang22@gmail.com>
2022-09-05 22:56:36 +0700

    Added browse page

--- client/src/pages/shop/Browse.js
+++ client/src/pages/shop/Browse.js
@@ -1,7 +1,15 @@
 import React from "react";
-
+import { BiFilter } from "react-icons/bi";
+import { Filter } from "../../components/BrowsePage/Filter";
 const Browse = () => {
-  return <div>Browse</div>;
+  return (
+    <div className="w-screen h-screen mt-[75px] ">
+      <main className="grid-cols-10 grid h-full">
+        <Filter />
+        <section className="lg:col-span-7"></section>
+      </main>
+    </div>
+  );
 };
 
 export default Browse;
